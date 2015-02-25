7eac96d6098d4d511b0669bd0b71e4a6306c22e6
Nutcharee <nutcharee.mainajang@gmail.com>
2015-02-25 13:57:31 -0800

    index.php new update

---
+++ update_v.1.2/index.php
@@ -0,0 +1,20 @@
+﻿<?php
+@mysql_connect("localhost","root","root");
+mysql_select_db("project3_se03");
+
+//ดึงข้อมูลออกมาในรูปแบบ UTF 8
+mysql_query("SET NAMES UTF8");
+
+$q=mysql_query("SELECT ActivityID, ActivityName, DetailActivity, Solution, Objective FROM activity");
+
+while($e=mysql_fetch_assoc($q))
+       $output[]=$e;
+
+function jsonRemoveUnicodeSequences($struct) {
+  return @preg_replace("/\\\\u([a-f0-9]{4})/e", "iconv('UCS-4LE','UTF-8',pack('V', hexdec('U$1')))", json_encode($struct));
+}   
+   
+//print(json_encode($output));
+print jsonRemoveUnicodeSequences($output);
+mysql_close();
+?>
