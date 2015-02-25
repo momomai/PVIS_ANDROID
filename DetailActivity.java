7c0f8cf4065e56e66bc1480029e32a7a046e79a1
Nutcharee <nutcharee.mainajang@gmail.com>
2015-02-25 11:12:31 -0800

    test commit&push

--- DetailActivity.java
+++ DetailActivity.java
@@ -1,15 +1,29 @@
-package com.psu.pvis_android;
+package com.psu.projectii;
+
+import java.util.ArrayList;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class DetailActivity extends Activity{
+import com.psu.helper.Config;
+import com.psu.helper.HttpHelper;
 
+public class DetailActivity extends Activity {
+	
 	private ImageButton ibBack;
 	private TextView tvActivityName, tvFac, tvContact;
 	private TextView tvTel, tvObjective, tvDetail;
@@ -17,24 +31,66 @@ public class DetailActivity extends Activity{
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.detil_activity);
+		setContentView(R.layout.detail_activity);
 		
-		ibBack = (ImageButton) findViewById(R.id.ibBack);
-		imActivity = (ImageView) findViewById(R.id.tv_Pic);
-		tvActivityName = (TextView) findViewById(R.id.tv_Name);
-		tvFac = (TextView) findViewById(R.id.tv_Fac);
-		tvContact = (TextView) findViewById(R.id.tv_Contact);
-		tvTel = (TextView) findViewById(R.id.tv_Tel);
-		tvObjective = (TextView) findViewById(R.id.tv_Object);
-		tvDetail = (TextView) findViewById(R.id.tv_Detail);
-		
-		ibBack.setOnClickListener(new OnClickListener() {
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy);
+			
+			ibBack = (ImageButton) findViewById(R.id.ibBack);
+			imActivity = (ImageView) findViewById(R.id.tv_Pic);
+			tvActivityName = (TextView) findViewById(R.id.tv_Name);
+			tvFac = (TextView) findViewById(R.id.tv_Fac);
+			tvContact = (TextView) findViewById(R.id.tv_Contact);
+			//tvTel = (TextView) findViewById(R.id.tv_Tel);
+			tvObjective = (TextView) findViewById(R.id.tv_Object);
+			//tvDetail = (TextView) findViewById(R.id.tv_Detail);
 
-			public void onClick(View v) {
-					finish();
+			Log.i("TEST","1");
+			
+			Intent intent= getIntent();
+			Log.i("TEST","2");
+			final String activityID = intent.getStringExtra("ActivityID");
+			Log.i("TEST","3");
+			ArrayList<NameValuePair> data = new ArrayList<NameValuePair>();
+			Log.i("TEST","4");
+			data.add(new BasicNameValuePair("ActivityID", activityID));
+
+			//data.add(new BasicNameValuePair("ActivityID", activityID.getString("ActivityID")));
+
+			String result = HttpHelper.GetString(String.format("http://%s/192.168.208.220/Rest/index.php?rnd=%s&r=%s", 
+					Config.Ip,Math.random()), data);
+
+			Log.i("RESULT", result);
+			
+			JSONArray jsArr;
+			try {
+				jsArr = new JSONArray(result);
+				if (jsArr.length() == 1) {
+					JSONObject jsObj = jsArr.getJSONObject(0);
+
+					tvActivityName.setText(jsObj.getString("ActivityName"));
+
+					tvFac.setText(jsObj.getString("DetailActivity"));
+					
+					tvContact.setText(jsObj.getString("Solution"));
+
+					//tvTel.setText(jsObj.getString("PAGES"));
+
+					tvObjective.setText(jsObj.getString("Objective"));
+
+					//tvDetail.setText(jsObj.getString("DetailActivity"));
+
+				}
+				ibBack.setOnClickListener(new OnClickListener() {
+
+					public void onClick(View v) {
+						finish();
+					}
+				});
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		});
 	}
 }
