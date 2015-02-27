99d00418cc8546a0017c769b639cc6b8d3f7d8eb
Terawat <make40501@gmail.com>
2015-02-27 09:34:07 +0700

    SearchUpdate

--- SearchActivity.java
+++ SearchActivity.java
@@ -13,10 +13,9 @@ import com.psu.entry.AddressEntry;
 import com.psu.helper.Config;
 import com.psu.helper.HttpHelper;
 
-
-
 import android.support.v7.app.ActionBarActivity;
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.StrictMode;
@@ -25,90 +24,109 @@ import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+@SuppressLint("NewApi")
+public class MainActivity extends ActionBarActivity {
 
-@SuppressLint("NewApi") public class MainActivity extends ActionBarActivity {
+	private Spinner spAumper, spTumbon;
+	private Button btnSearch, btnReset;
+	private ArrayList<AddressEntry> listdata = new ArrayList<AddressEntry>();
+	private ArrayList<AddressEntry> listdatas = new ArrayList<AddressEntry>();
 
-	private Spinner spAumper,spTumbon;
-	private ArrayList<AddressEntry> items,items2;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (android.os.Build.VERSION.SDK_INT > 9) {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-        }
-        Log.i("FINDVIEW", "lvResult1");
-        	
-       spAumper = (Spinner)findViewById(R.id.spAumper);
-        spTumbon = (Spinner)findViewById(R.id.spTambon);
-        Log.i("FINDVIEW", "lvResult2");
-        
-        String result = HttpHelper.GetString(String.format("http://%s/testsql2.php", Config.Ip), null);
-        Log.i("RESULT", result);
-        //String url = "http://%s/testsql.php";
-        
-       try {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		if (android.os.Build.VERSION.SDK_INT > 9) {
+			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+					.permitAll().build();
+			StrictMode.setThreadPolicy(policy);
+		}
+		Log.i("FINDVIEW", "lvResult1");
+
+		spAumper = (Spinner) findViewById(R.id.spAumper);
+		spTumbon = (Spinner) findViewById(R.id.spTambon);
+		btnSearch = (Button) findViewById(R.id.btnSearch);
+		btnReset = (Button) findViewById(R.id.btnReset);
+
+		Log.i("FINDVIEW", "lvResult2");
+
+		String result = HttpHelper.GetString(
+				String.format("http://%s/testsql2.php", Config.Ip), null);
+		Log.i("RESULT", result);
+		// String url = "http://%s/testsql.php";
+
+		try {
 			JSONArray JSArr = new JSONArray(result);
-			items = new ArrayList<AddressEntry>();
+			listdata = new ArrayList<AddressEntry>();
 			Log.i("FINDVIEW", "lvResult4");
 			for (int i = 0; i < JSArr.length(); i++) {
-				
+
 				JSONObject JSObj = JSArr.getJSONObject(i);
-				
-				items.add(new AddressEntry(JSObj.getString("District")));
-				//items.add(new AddressEntry(JSObj.getString("Tumbol")));
+
+				listdata.add(new AddressEntry(JSObj.getString("District")));
+				// items.add(new AddressEntry(JSObj.getString("Tumbol")));
 				Log.i("FINDVIEW", "lvResult5");
 			}
 			spAumper.setAdapter(new AumAdapter());
 			spTumbon.setAdapter(new AumAdapter());
-			
+
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			Log.e("JSON Parser", "Error parsing data " + e.toString());
 			e.printStackTrace();
-			
+
 		}
-       String result2 = HttpHelper.GetString(String.format("http://%s/testsql.php", Config.Ip), null);
-       
-       try {
+		String result2 = HttpHelper.GetString(
+				String.format("http://%s/testsql.php", Config.Ip), null);
+
+		try {
 			JSONArray JSArr = new JSONArray(result2);
-			items2 = new ArrayList<AddressEntry>();
+			listdatas = new ArrayList<AddressEntry>();
 			Log.i("FINDVIEW", "lvResult4");
 			for (int i = 0; i < JSArr.length(); i++) {
-				
+
 				JSONObject JSObj = JSArr.getJSONObject(i);
-				
-				
-				items2.add(new AddressEntry(JSObj.getString("Tumbol")));
+
+				listdatas.add(new AddressEntry(JSObj.getString("Tumbol")));
 				Log.i("FINDVIEW", "lvResult5");
 			}
-			
+
 			spTumbon.setAdapter(new TumAdapter());
-			
+
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			Log.e("JSON Parser", "Error parsing data " + e.toString());
 			e.printStackTrace();
-			
+
 		}
-        
-       
-    }
-    public class AumAdapter extends BaseAdapter{
-    	private Holder holder;
-    	
+
+		btnReset.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+	}
+
+	public class AumAdapter extends BaseAdapter {
+		private Holder holder;
+
 		@Override
 		public int getCount() {
 			// TODO Auto-generated method stub
-			return items.size();
+			return listdata.size();
 		}
 
 		@Override
@@ -126,7 +144,7 @@ import android.widget.TextView;
 		@Override
 		public View getView(int position, View v, ViewGroup vg) {
 			// TODO Auto-generated method stub
-
+			final AddressEntry item = listdata.get(position);
 			if (v == null) {
 				Log.i("FINDVIEW", "lvResult8");
 				LinearLayout ll = new LinearLayout(getApplicationContext());
@@ -146,23 +164,25 @@ import android.widget.TextView;
 				holder = (Holder) v.getTag();
 			}
 			Log.i("FINDVIEW", "lvResult8");
-			holder.Title.setText(items.get(position).getDistrict());
-			return v;
-		}
-		}
-    
-	/*private Collection getJSONUrl(String url) {
-		// TODO Auto-generated method stub
-		return null;
-	}*/
-    public class TumAdapter extends BaseAdapter{
+			holder.Title.setText(listdata.get(position).getDistrict());
 
-private Holder holder;
-    	
+			return v;
+
+		}
+	}
+
+	/*
+	 * private Collection getJSONUrl(String url) { // TODO Auto-generated method
+	 * stub return null; }
+	 */
+	public class TumAdapter extends BaseAdapter {
+
+		private Holder holder;
+
 		@Override
 		public int getCount() {
 			// TODO Auto-generated method stub
-			return items2.size();
+			return listdatas.size();
 		}
 
 		@Override
@@ -180,7 +200,7 @@ private Holder holder;
 		@Override
 		public View getView(int position, View v, ViewGroup vg) {
 			// TODO Auto-generated method stub
-
+			final AddressEntry item = listdatas.get(position);
 			if (v == null) {
 				Log.i("FINDVIEW", "lvResult8");
 				LinearLayout ll = new LinearLayout(getApplicationContext());
@@ -200,14 +220,27 @@ private Holder holder;
 				holder = (Holder) v.getTag();
 			}
 			Log.i("FINDVIEW", "lvResult8");
-			holder.Title2.setText(items2.get(position).getDistrict());
+			holder.Title2.setText(listdatas.get(position).getDistrict());
+
+			btnSearch.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+
+					Intent intent = new Intent(MainActivity.this,
+							ActivityListActivity.class);
+					intent.putExtra("addressID", item.getAddressID());
+					startActivity(intent);
+				}
+			});
 			return v;
 		}
-    	
-    }
 
-   public class Holder {
-	   public TextView Title,Title2;
-	   
-   }
+	}
+
+	public class Holder {
+		public TextView Title, Title2;
+
+	}
 }
