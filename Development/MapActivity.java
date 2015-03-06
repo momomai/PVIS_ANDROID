53841d308ffe28c289a1c77e3eaa39a5ab0161a6
Nutcharee <nutcharee.mainajang@gmail.com>
2015-03-06 10:59:37 -0800

    map connect

---
+++ Development/MapActivity.java
@@ -0,0 +1,14 @@
+package com.psu.project_pvis;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MapActivity extends FragmentActivity{
+
+	@Override
+	protected void onCreate(Bundle arg0) {
+		// TODO Auto-generated method stub
+		super.onCreate(arg0);
+		setContentView(R.layout.map_activity);
+	}
+}
